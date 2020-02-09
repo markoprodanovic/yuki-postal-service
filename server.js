277d0fc77685cf485c94d691cb2dc5df0336f8d3
Marko Prodanovic <marko.prodanovic@outlook.com>
2020-02-08 18:00:31 -0800

    Added catchall for server. React app now being served on '/'

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ app.get('/api/posts', (req, res) => {
     res.json(posts)
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
