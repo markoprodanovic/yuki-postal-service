1c4bb9ee9266e84b6180582120d5f049e71993c0
Marko Prodanovic <marko.prodanovic@outlook.com>
2020-02-08 17:41:43 -0800

    Added procfile and instructed server to get port number from host

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.get('/api/posts', (req, res) => {
     res.json(posts)
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
